c62c29c6276655f4552feb997c53efbbade3322c
dajhelyan <assiaxramx10@gmail.com>
2019-11-11 07:58:11 -0500

    implementando funcion handleChange que escucha cada cambio en el input y funcion que contiene fetch api, agregandola como prop a button consoleando resultados de busqueda... Fixed #11

--- pinterest/src/components/PinterestApp.js
+++ pinterest/src/components/PinterestApp.js
@@ -5,9 +5,9 @@ const api = `https://api.unsplash.com/photos/?client_id=${id}&per_page=20`;
 
 const PinterestApp = (props) => {
 
-	// trayendo elementos y almacenandolos en una constante
+    // trayendo elementos y almacenandolos en una constante
     const initialState = useFetchApi(api)
-    
+
 
     return (
         <React.Fragment>
@@ -15,23 +15,23 @@ const PinterestApp = (props) => {
                 <Search />
             </Header>
             <p>hola {props.author}</p>
-  
+
             {/* iterando dentro de componente para agregar un componente de cada imagen por iteracion */}
             <ContainerImg>
-				{
-					initialState.map((item) => {
-						return <CardItem key={item.id} {...item} />
-					})
-				} 
-			</ContainerImg>
-            
+                {
+                    initialState.map((item) => {
+                        return <CardItem key={item.id} {...item} />
+                    })
+                }
+            </ContainerImg>
+
         </React.Fragment>
     )
 }
 
 
 
-const Header = ({children}) => {
+const Header = ({ children }) => {
     return (
         <header>
             <figure>
@@ -44,26 +44,59 @@ const Header = ({children}) => {
     )
 }
 
-const Search = ({query}) => {
+const Search = () => {
+
+    const [query, setQuery] = useState('');
+
+    // const apiSearch = `https://api.unsplash.com/search/photos?&query=${query}&client_id=${id}&per_page=20`
+
+
+    const searching = () => {
+        fetch(`https://api.unsplash.com/search/photos?&query=${query}&client_id=${id}&per_page=20`)
+        .then(res => {
+            return res.json()
+        }).then((result) => {
+            console.log(result)
+        })
+       
+    }
+
+    /* generando un evento y controlando con onclick */
+    const handleClick = (e) => {
+        setQuery(e.target.value);
+        console.log(setQuery, 'oii')
+    }
+
+    /* const handleSubmit = (e) => {
+        e.preventDefault(e)
+        useFetchApi(apiSearch)
+    } */
+
     return (
         <div>
-            <input placeholder="Buscar" value={query}/>
-            <button>Buscar</button>
+            <input type="text" 
+                onChange={handleClick}
+                placeholder="Buscar"
+                value={query}
+            />
+            <button onClick={searching} >Buscar</button>
         </div>
     )
 }
 
 
+
+
 // componente contenedor de imagenes, recibiendo como prop un children(que recibe en el componente app una iteracion con map que retrona un componentecon cada imagen)
-const ContainerImg = ({children}) => {
-    
+const ContainerImg = ({ children }) => {
+
     return (
         <section>
-          <div className="container">
-            <div className="card-columns">
-              {children}
+            <div className="container">
+                <div className="card-columns">
+                    {children}
+                </div>
             </div>
-          </div>
         </section>
     )
 
@@ -71,26 +104,26 @@ const ContainerImg = ({children}) => {
 
 // componente card por imagen que recibe como prop una todo item(data)
 const CardItem = ({ ...item }) => {
-	return(
-	<div className="card">
-	{/* inseratando componente imagen mandando de prop link de cada imagen y descripcion */}
-		<Image url={item.urls.raw} alt={item.alt_description} />
-	</div>
-	)
+    return (
+        <div className="card">
+            {/* inseratando componente imagen mandando de prop link de cada imagen y descripcion */}
+            <Image url={item.urls.raw} alt={item.alt_description} />
+        </div>
+    )
 }
 
 // componente imagen que recibe url de imagen para pintar
 const Image = ({ url, alt }) => {
-	return (
-		<img src={url} alt={alt} height="100" />
-	
-	)
-                
+    return (
+        <img src={url} alt={alt} height="100" />
+
+    )
+
 }
- 
+
 // construyendo un hook que hace fetch api
 const useFetchApi = (api) => {
-    const [imagenes, setImagenes] = useState([])
+    const [data, setData] = useState([])
 
     useEffect(() => {
         // agrengando parametro que trae 20 imagenes por pagina
@@ -99,14 +132,14 @@ const useFetchApi = (api) => {
                 return response.json();
             })
             .then((data) => {
-                setImagenes(data);
+                setData(data);
             });
     }, [])
-    console.log(imagenes, 'popo');
-    return imagenes;
+    console.log(data, 'popo');
+    return data;
 }
 
-	
+
 
 
 
