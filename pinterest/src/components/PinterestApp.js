be3496a70ddb310c0a5e25bfba10a5459df650f5
dajhelyan <assiaxramx10@gmail.com>
2019-11-12 15:25:29 -0500

    levantando estado para actualizar la data

--- pinterest/src/components/PinterestApp.js
+++ pinterest/src/components/PinterestApp.js
@@ -1,24 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 const id = '4c974f2e37b1799fdb6e91a0f891a25df26b687e9e6eb77816d9988dd5142e59';
 const api = `https://api.unsplash.com/photos/?client_id=${id}&per_page=20`;
 
-const PinterestApp = (props) => {
-
+const PinterestApp = () => { //estado inicial
+    const [initialState, setState] = useState([]) 
     // trayendo elementos y almacenandolos en una constante
-    const initialState = useFetchApi(api)
 
+
+    
+
+    useEffect(() => {
+        fetch(`https://api.unsplash.com/photos/?client_id=${id}&per_page=20`)
+            .then(res => {
+                return res.json()
+            }).then((data) => {
+                setState(data)                
+            })
+    },[])
+
+    
 
     return (
         <React.Fragment>
             <Header>
-                <Search />
+                <Search actualizarEstado={setState}/>
             </Header>
-            <p>hola {props.author}</p>
-
             {/* iterando dentro de componente para agregar un componente de cada imagen por iteracion */}
             <ContainerImg>
-                {
+                {// implementare ternario con condicion
                     initialState.map((item) => {
                         return <CardItem key={item.id} {...item} />
                     })
@@ -44,27 +55,28 @@ const Header = ({ children }) => {
     )
 }
 
-const Search = () => {
-
+const Search = (props) => {
+    // declarando los estados
     const [query, setQuery] = useState('');
 
     // const apiSearch = `https://api.unsplash.com/search/photos?&query=${query}&client_id=${id}&per_page=20`
 
-
+    // funcion que ejecuta la llamada a la api (search)
     const searching = () => {
         fetch(`https://api.unsplash.com/search/photos?&query=${query}&client_id=${id}&per_page=20`)
-        .then(res => {
-            return res.json()
-        }).then((result) => {
-            console.log(result)
-        })
-       
+            .then(res => {
+                return res.json()
+            }).then((result) => {
+                props.actualizarEstado(result)
+                console.log(result)
+            })
+
     }
 
-    /* generando un evento y controlando con onclick */
-    const handleClick = (e) => {
+    /* metodo que se ejecuta cuando escucha un cambio en input */
+    const handleChange = (e) => {
         setQuery(e.target.value);
-        console.log(setQuery, 'oii')
+        // console.log(setQuery, 'oii')
     }
 
     /* const handleSubmit = (e) => {
@@ -74,8 +86,8 @@ const Search = () => {
 
     return (
         <div>
-            <input type="text" 
-                onChange={handleClick}
+            <input type="text"
+                onChange={handleChange}
                 placeholder="Buscar"
                 value={query}
             />
@@ -92,10 +104,8 @@ const ContainerImg = ({ children }) => {
 
     return (
         <section>
-            <div className="container">
-                <div className="card-columns">
-                    {children}
-                </div>
+            <div className="card-columns">
+                {children}
             </div>
         </section>
     )
@@ -104,40 +114,83 @@ const ContainerImg = ({ children }) => {
 
 // componente card por imagen que recibe como prop una todo item(data)
 const CardItem = ({ ...item }) => {
+    
+
+
+    // const [ on, setOn ] = useState(true)
+
+    /* const handleClick = (e) => {
+        console.log(e.target, 'click');
+
+    } */
+
     return (
-        <div className="card">
+        <div className="card" >
             {/* inseratando componente imagen mandando de prop link de cada imagen y descripcion */}
             <Image url={item.urls.raw} alt={item.alt_description} />
+            
         </div>
     )
 }
 
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p> 
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
+  
+
+
+
 // componente imagen que recibe url de imagen para pintar
 const Image = ({ url, alt }) => {
     return (
-        <img src={url} alt={alt} height="100" />
+        <img src={url} alt={alt} />
 
     )
 
 }
 
 // construyendo un hook que hace fetch api
-const useFetchApi = (api) => {
+/* const useFetchApi = (Api) => {
     const [data, setData] = useState([])
 
-    useEffect(() => {
+
         // agrengando parametro que trae 20 imagenes por pagina
-        fetch(api)
+        fetch(Api)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
                 setData(data);
             });
-    }, [])
+ 
     console.log(data, 'popo');
     return data;
-}
+} */
 
 
 
